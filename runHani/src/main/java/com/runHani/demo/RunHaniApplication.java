83e488dc1d8f6a68b62d1122c5c1105c5a7aad70
wstudee <wstudlee@gmail.com>
2022-01-05 22:39:04 +0900

    firstCommit - RunHani Run !

---
+++ runHani/src/main/java/com/runHani/demo/RunHaniApplication.java
@@ -0,0 +1,13 @@
+package com.runHani.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunHaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunHaniApplication.class, args);
+	}
+
+}
---
+++ runHani/src/main/java/com/runHani/demo/TestController.java
@@ -0,0 +1,15 @@
+package com.runHani.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @GetMapping("/")
+  public String index() {
+    return "demo project!!";
+  }
+}
+
+
