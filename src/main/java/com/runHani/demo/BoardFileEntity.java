be8dcd5b9cee4810a61b9975279af1f3b859554e
Mira <nkskjjbb@gmail.com>
2022-01-12 20:53:52 +0900

    엔티티추가

---
+++ src/main/java/com/runHani/demo/BoardFileEntity.java
@@ -0,0 +1,30 @@
+package com.runHani.demo;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_boardfile_bas")
+public class BoardFileEntity {
+
+	@Id
+	@Column(name = "attached_file_no", nullable = false, updatable = true, length = 20)
+	private String attachedFileNo    ;
+	
+	@Column(name = "file_name", nullable = false, updatable = true, length = 150) 
+	private String fileName           ;
+
+	@Column(name = "file_save_name", nullable = false, updatable = true, length = 1000) 
+	private String fileSaveName      ;
+	
+	@Column(name = "reg_user", nullable = false, updatable = true, length = 150)
+	private String regUser     ;
+
+	@Column(name = "reg_date", nullable = false, updatable = true)
+	private LocalDateTime regDate     = LocalDateTime.now() ;
+	
+}
---
+++ src/main/java/com/runHani/demo/CodeEntity.java
@@ -0,0 +1,42 @@
+package com.runHani.demo;
+
+import java.time.LocalDateTime;
+import javax.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_common_cod")
+public class CodeEntity {
+
+	@Id
+	@Column(name = "code_cl_code", nullable = false, updatable = true , length  = 10)
+	private String codeClCode ;
+	
+	@Column(name = "code_cl_name", nullable = false, updatable = true , length  = 50)
+	private String codeClName ;
+	
+	@Column(name = "code", nullable = false, updatable = true , length  = 10)
+	private String code         ;
+	
+	@Column(name = "code_name", nullable = false, updatable = true,  length  = 50)
+	private String codeName    ;
+	
+	@Column(name = "code_info", nullable = false, updatable = true, length  = 500)
+	private String codeInfo    ;
+
+	@Column(name = "update_user", nullable = false, updatable = true, length  = 150)
+	private String updateUser  ;
+
+	@Column(name = "update_date", nullable = false, updatable = true)
+	private LocalDateTime updateDate = LocalDateTime.now() ;
+	
+	@Column(name = "reg_user", nullable = false, updatable = true, length  = 150)
+	private String regUser     ;
+
+	@Column(name = "reg_date", nullable = false, updatable = true)
+	private LocalDateTime regDate     = LocalDateTime.now() ;
+	
+}
