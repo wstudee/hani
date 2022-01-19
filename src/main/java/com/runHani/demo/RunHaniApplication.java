b4c25cdf2c0a84b7c2de1911d72afd2fd1df8d50
Mira <nkskjjbb@gmail.com>
2022-01-19 22:39:04 +0900

    fix : 엔티티 패키지 위치 변경 및

--- src/main/java/com/runHani/demo/RunHaniApplication.java
+++ src/main/java/com/runHani/demo/RunHaniApplication.java
@@ -1,9 +1,13 @@
 package com.runHani.demo;
 
+import javax.persistence.Entity;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.runHani")
 public class RunHaniApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/runHani/entity/NoticeFileEntity.java
+++ src/main/java/com/runHani/entity/NoticeFileEntity.java
@@ -1,15 +1,18 @@
-package com.runHani.demo;
+package com.runHani.entity;
 
 import java.time.LocalDateTime;
 import javax.persistence.*;
+
+import com.sun.istack.NotNull;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "tb_boardfile_bas")
-public class BoardFileEntity {
+@Table(name = "tb_noticefile_bas")
+public class NoticeFileEntity {
 
 	@Id
 	@Column(name = "attached_file_no", nullable = false, updatable = true, length = 20)
@@ -21,9 +24,10 @@ public class BoardFileEntity {
 	@Column(name = "file_save_name", nullable = false, updatable = true, length = 1000) 
 	private String fileSaveName      ;
 	
+	@NotNull
 	@ManyToOne
 	@JoinColumn(name ="reg_user", nullable = false)
-	private UserEntity regUser;
+	private UserEntity user;
 
 	@Column(name = "reg_date", nullable = false, updatable = true)
 	private LocalDateTime regDate     = LocalDateTime.now() ;
