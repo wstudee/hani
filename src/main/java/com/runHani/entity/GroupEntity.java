dfac6fd9fc71c756dde30aeae83fbb1ca425c8b2
Mira <nkskjjbb@gmail.com>
2022-05-07 21:52:04 +0900

    feat :  그룹 관련 테이블 생성

---
+++ src/main/java/com/runHani/entity/GroupEntity.java
@@ -0,0 +1,45 @@
+package com.runHani.entity;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.runHani.repository.BoardFileRepository;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_group_bas")
+@Data
+public class GroupEntity {
+
+	@Id
+	@Column(name = "sn", nullable = false, updatable = true, length = 20)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer sn    ;
+
+	@Column(name = "group_name", nullable = false, updatable = true, length =50)
+	private String groupName;
+	
+	@Column(name = "member_count", nullable = false, updatable = true, length =50)
+	private int memberCount ;
+		
+	@Column(name = "group_status", nullable = false, updatable = true, length =50)
+	private String groupStatus;
+	
+	
+	@Column(name = "update_cnt", nullable = false, updatable = true)
+	private int updateCnt;
+
+	@ManyToOne
+	@JoinColumn(name ="leader", nullable = false, updatable = true)
+	private UserEntity leader;
+	
+}
---
+++ src/main/java/com/runHani/entity/UserGroupEntity.java
@@ -0,0 +1,36 @@
+package com.runHani.entity;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.ColumnDefault;
+
+import com.runHani.repository.BoardFileRepository;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usergroup_bas")
+@Data
+public class UserGroupEntity {
+
+	@Id
+	@Column(name = "sn", nullable = false, updatable = true, length = 20)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer sn;
+
+	@ManyToOne
+	@JoinColumn( referencedColumnName = "sn" , name = "group_sn")
+	private GroupEntity groupSn;
+	 
+	@ManyToOne
+	@JoinColumn(name ="email", nullable = false, updatable = true)
+	private UserEntity user;
+	
+}
